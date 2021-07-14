3c631844b3ffba72d82208ff984e04ac924edbc0
18581489108 <18581489108@163.com>
2021-07-14 19:30:28 +0800

    支持按歌手、专辑名加载歌单
    * 根据 album_url 来加载专辑列表
    * 根据 artist_url 来加载歌手的歌曲列表

--- js/provider/directlink.js
+++ js/provider/directlink.js
@@ -25,13 +25,45 @@ class directlink {
       switch (list_id) {
         case 'dlplaylist':
           return this.dl_get_playlist(url);
+        case 'dlalbum':
+          return this.dl_album(url);
+        case 'dlartist':
+          return this.dl_artist(url);
         default:
           return null;
       }
     }
 
     static dl_get_playlist(url) {
-      const playlist_url = getParameterByName('list_id', url).split('dlplaylist_').pop();
+      return this.loadResult(url, 'dlplaylist_');
+    }
+    
+    static dl_album(url) {
+      return this.loadResult(url, 'dlalbum_');
+    }
+
+    static dl_artist(url) {
+      return this.loadResult(url, 'dlartist_');
+    }
+
+    static dl_convert_song(song_info) {
+      const track = {
+        id: `dltrack_${song_info.id}`,
+        title: song_info.title,
+        artist: song_info.artist,
+        artist_id: `dlartist_${song_info.artist_url}`,
+        album: song_info.album,
+        album_id: `dlalbum_${song_info.album_url}`,
+        source: 'directlink',
+        source_url: song_info.source_url,
+        img_url: song_info.cover,
+        lyric_url: song_info.lyric_url,
+      };
+      return track;
+    }
+
+    static loadResult(url, prefix) {
+      const playlist_url = getParameterByName('list_id', url).split(prefix).pop();
       return {
         success: (fn) => {
           axios.get(playlist_url).then((response) => {
@@ -39,7 +71,7 @@ class directlink {
             const info = {
               cover_img_url: data.cover,
               title: data.title,
-              id: `dlplaylist_${playlist_url}`,
+              id: `${prefix}${playlist_url}`,
               source_url: `${playlist_url}`,
             };
             const tracks = data.tracks.map((item) => this.dl_convert_song(item));
@@ -52,21 +84,7 @@ class directlink {
       };
     }
 
-    static dl_convert_song(song_info) {
-      const track = {
-        id: `dltrack_${song_info.id}`,
-        title: song_info.title,
-        artist: song_info.artist,
-        artist_id: `dlartist_${song_info.artist_id}`,
-        album: song_info.album,
-        album_id: song_info.album_id,
-        source: 'directlink',
-        source_url: song_info.source_url,
-        img_url: song_info.cover,
-        lyric_url: song_info.lyric_url,
-      };
-      return track;
-    }
+
 
     static bootstrap_track(track, success, failure) {
         const sound = {};
